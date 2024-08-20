0d007e86315b3daff5aec221637e1ac2f40dce66
vedkerkar10 <113177694+vedkerkar10@users.noreply.github.com>
2024-08-20 23:45:48 +0530

    final

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import TextInput from "./components/TextInput";
 
@@ -12,10 +12,20 @@ export default function Home() {
 
   const [extractedKeywords, setExtractedKeywords] = useState<{ [key: string]: string }>({});
   const [extractedText, setExtractedText] = useState<string>("");
+  const [domain, setDomain] = useState("");
+
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_DOMAIN) {
+      setDomain(process.env.NEXT_PUBLIC_DOMAIN);
+    }
+  },
+  []);
+
+  console.log(domain);
 
   const handleTextSubmit = async (text: string) => {
     try {
-      const response = await axios.post("http://localhost:5000/extract", { text });
+      const response = await axios.post(`${domain}/extract`, { text });
       setExtractedKeywords(response.data);
     } catch (error) {
       console.error("Error extracting keywords:", error);
